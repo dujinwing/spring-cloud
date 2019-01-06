6e32fde561bb795872b12d42577ce22856455fe9
dujinwing <631122164@qq.com>
2019-01-06 22:20:41 +0800

    config

---
+++ spring-boot/config-client/src/main/java/com/bsoft/configclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.bsoft.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
+
